bdc4f11968a34ac253a5219a3137fd573be58f7b
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2013-06-10 14:30:54 -0500

    pushing new content

--- presentations/library/prace_shortcourse_2013/snippets/es.cxx
+++ presentations/library/prace_shortcourse_2013/snippets/es.cxx
@@ -1,11 +1,4 @@
   ...
-  // Create a mesh.
-  Mesh mesh;
-  
-  // Create a uniform 5x5 mesh on the unit square.
-  MeshTools::Generation::build_square (mesh, 5, 5);
-  mesh.print_info();
-
   // Create an equation systems object. This object can contain
   // multiple systems of different flavors for solving loosely coupled
   // physics.  Each system can contain multiple variables of different
@@ -13,12 +6,12 @@
   // reference to the mesh object, so the order of construction here
   // is important.
   EquationSystems equation_systems (mesh);
-  
+
   // Now we declare the system and its variables.  We begin by adding
   // a "TransientLinearImplicitSystem" to the EquationSystems object,
   // and we give it the name "Simple System".
   equation_systems.add_system<TransientLinearImplicitSystem> ("Simple System");
-    
+
   // Adds the variable "u" to "Simple System".  "u" will be
   // approximated using first-order approximation.
   equation_systems.get_system("Simple System").add_variable("u", FIRST);
@@ -34,10 +27,10 @@
   equation_systems.get_system("Complex System").add_variable("c", FIRST);
   equation_systems.get_system("Complex System").add_variable("T", FIRST);
   equation_systems.get_system("Complex System").add_variable("dv", SECOND, MONOMIAL);
-    
+
   // Initialize the data structures for the equation system.
   equation_systems.init();
-        
+
   // Prints information about the system to the screen.
   equation_systems.print_info();
   ...
