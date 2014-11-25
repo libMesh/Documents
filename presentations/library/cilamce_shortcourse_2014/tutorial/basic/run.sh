7ff2b9cfb2054e83b834e771a877bc06eb972926
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2014-11-25 11:56:14 -0300

    fixes for libmesh-0.9.4-rc1

--- presentations/library/cilamce_shortcourse_2014/tutorial/basic/run.sh
+++ presentations/library/cilamce_shortcourse_2014/tutorial/basic/run.sh
@@ -6,8 +6,8 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=introduction_ex1
 
-options="-d 3 $LIBMESH_DIR/reference_elements/3D/one_hex27.xda"
+options="-d 2 $LIBMESH_DIR/share/reference_elements/2D/one_tri6.xda"
 run_example "$example_name" "$options"
 
-options="-d 3 $LIBMESH_DIR/reference_elements/3D/one_hex27.xda -o output.xda"
+options="-d 3 $LIBMESH_DIR/share/reference_elements/3D/one_hex27.xda -o output.xda"
 run_example "$example_name" "$options"
